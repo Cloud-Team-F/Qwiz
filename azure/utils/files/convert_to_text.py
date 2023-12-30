09dd8df38806c2632bd3b7f9aecb9228f5ef7ce3
Alvis <37454764+avibn@users.noreply.github.com>
2023-12-30 12:34:45 +0000

    clean up file conversion

--- azure/utils/files/convert_to_text.py
+++ azure/utils/files/convert_to_text.py
@@ -1,10 +1,12 @@
 import logging
+import os
 import re
 import string
-import fitz
-import textract
-import requests
 import tempfile
+
+import fitz
+import requests
+import textract
 
 
 def convert_to_text(file_url: str, mime: str) -> str:
@@ -19,77 +21,101 @@ def convert_to_text(file_url: str, mime: str) -> str:
         str: The converted text.
     """
     logging.info(f"Converting file to text: {file_url}, {mime}")
-    
+
     try:
-        response = requests.get(file_url) # http request to get the file 
-        response.raise_for_status() # check if successful before continuing
-        file = response.content # retrieve raw content of the file
-        
-        # check file extension --> different method for different file type
-        # could've used mime type? maybe? perhaps? perchance?
-        file_extension = file_url.split('.')[-1].lower() 
-        
-        
-        # saves the file temporarily so that it can be passed into the functions
-        # 'file' is raw content so difficult to work with
-        with tempfile.NamedTemporaryFile(delete=False, suffix=f".{file_extension}") as temp_file:
+        response = requests.get(file_url)  # http request to get the file
+        response.raise_for_status()  # check if successful before continuing
+        file = response.content  # retrieve raw content of the file
+
+        # Determine file extension based on MIME type
+        match mime.lower():
+            case "application/pdf":
+                file_extension = "pdf"
+            case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+                file_extension = "docx"
+            case "application/vnd.openxmlformats-officedocument.presentationml.presentation":
+                file_extension = "pptx"
+            case _:
+                logging.error(f"Unknown MIME type: {mime}")
+                return ""
+
+        # Create temporary file to store the file
+        with tempfile.NamedTemporaryFile(
+            suffix=f".{file_extension}", delete=False
+        ) as temp_file:
+            # Write file to temporary file
             temp_file.write(file)
-        
-        # specific functions for PDF and DOC files
-        # as they don't work with textract because why would they
-        if file_extension == 'pdf': 
-            text = text_pdf(temp_file.name)
-        elif file_extension == 'doc':  # DOC DOES NOT WORK, I'M TOO INCOMPETENT TO MAKE IT WORK
-            text = text_doc(temp_file.name)
-        else:
-            text = textract.process(temp_file.name).decode('utf-8') # textract library works flawlessly with DOCX and PPTX
-        
-        cleaned_text = clean_text(text) # remove all unnecessary characters
-        
-        logging.info(f"Text successfully extracted from '{file_url}")
-        
-        return cleaned_text
-    
-    
+            temp_file.seek(0)
+            logging.info(f"File successfully saved path: {temp_file.name}")
+
+            # specific functions for PDF as they don't work with textract
+            if file_extension == "pdf":
+                text = text_pdf(temp_file.name)
+            else:
+                # textract library works flawlessly with DOCX and PPTX
+                text = textract.process(temp_file.name).decode("utf-8")
+
+            # remove all unnecessary characters
+            cleaned_text = clean_text(text)
+            logging.info(f"Text successfully extracted from '{file_url}")
+
+            # Clean up
+            temp_file.close()
+            os.remove(temp_file.name)
+            logging.info(f"Temporary file '{temp_file.name}' deleted")
+
+            return cleaned_text
+
     except Exception as e:
         logging.error(f"Error converting file to text: {e}")
         return ""
 
 
-def text_doc(file):
-    # spent too much time trying to make this work, i think i may be brain dead
-    return "" # i hate doc i hate doc i hate doc i hate doc i hate doc
-
 def text_pdf(file):
-    text = "" # initialise list of text
+    # Initialise list of text
+    text = ""
     pdf_document = fitz.open(file)
-    for page_number in range(pdf_document.page_count): # for every page of the pdf
-        page = pdf_document[page_number] 
-        text += page.get_text() # add the page text to the running list
-    pdf_document.close() # for safety idk
+
+    # for every page of the pdf
+    for page_number in range(pdf_document.page_count):
+        page = pdf_document[page_number]
+        text += page.get_text()  # add the page text to the running list
+
+    # Clean up
+    pdf_document.close()
     return text
+
 
 def clean_text(raw_text):
     # Remove special characters (except from punctuation), whitespaces, line breaks
-    cleaned_text = re.sub(r'[^a-zA-Z0-9\s' + re.escape(string.punctuation) + ']', '', raw_text)
-    
+    cleaned_text = re.sub(
+        r"[^a-zA-Z0-9\s" + re.escape(string.punctuation) + "]", "", raw_text
+    )
+
     # Remove conseuctive whitespaces
-    cleaned_text = re.sub(r'\s+', ' ', cleaned_text)
-    
+    cleaned_text = re.sub(r"\s+", " ", cleaned_text)
+
     # Remove leading/trailing white space
     return cleaned_text.strip()
 
 
-
-
-#testing code:
-
-# PDF file  : 
-#print(convert_to_text("https://www.africau.edu/images/default/sample.pdf","idk"))
-
-# PPTX file : 
-#print(convert_to_text("https://www.dickinson.edu/download/downloads/id/1076/sample_powerpoint_slides.pptx","idk"))
-
-# DOCX file : 
-#print(convert_to_text("https://calibre-ebook.com/downloads/demos/demo.docx","idk"))
-    
+# For testing
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    print(
+        convert_to_text(
+            "https://www.africau.edu/images/default/sample.pdf", "application/pdf"
+        )
+    )
+    print(
+        convert_to_text(
+            "https://www.dickinson.edu/download/downloads/id/1076/sample_powerpoint_slides.pptx",
+            "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+        )
+    )
+    print(
+        convert_to_text(
+            "https://calibre-ebook.com/downloads/demos/demo.docx",
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        )
+    )
