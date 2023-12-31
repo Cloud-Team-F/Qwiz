4c565a46f036d2f0cba1628ce4ec34634a450f30
williamMCgithub <46975781+williamMCgithub@users.noreply.github.com>
2023-12-31 18:41:02 +0000

    Update create_quiz.py
    Removed the marking functions. Idk why i left them here in the first place. 
    (they were already present in the Quiz_marking.py)

--- azure/utils/gpt/create_quiz.py
+++ azure/utils/gpt/create_quiz.py
@@ -369,43 +369,6 @@ def messageFillBlanks2(
 
     return completion
 
-
-def markShortAnswer(
-    question, answer
-):  # gives chatGPT a question and the student's answer and asks chatGPT to give a mark (correct/incorrect) and feedback.
-    client = OpenAI()
-    myMessageSystem = "You are a teacher, look at the short-answer question provided and the student's answer. You must respond in the following way: say either Correct or Incorrect. Then explain why the student was right/wrong."
-
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": myMessageSystem},
-            {
-                "role": "user",
-                "content": "Question: " + question + "\n Answer: " + answer,
-            },
-        ],
-    )
-
-    return completion
-
-
-def parseMarking(
-    response_text,
-):  # takes chatGPT's marking and formats it into a Submission object.
-    if response_text.startswith("Correct"):
-        mark = "Correct"
-    elif response_text.startswith("Incorrect"):
-        mark = "Incorrect"
-    else:
-        mark = "unknown"
-
-    # The feedback is the rest of the response after the mark
-    feedback = response_text.partition(". ")[2]
-
-    submission = Submission(mark, feedback)
-    return submission
-
     # This code will ask you to enter a question and an answer, chatGPT will mark you and the submission object will be created and printed.
     """
     myQuestion = input("enter a question: ")
